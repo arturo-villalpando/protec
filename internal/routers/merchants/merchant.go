e174054b3b46b58d992fae8c11b35ce8aef1d74d
Arturo Villalpando <arturo.villalpando.s@gmail.com>
2024-05-23 11:13:22 -0600

    update
    update

--- internal/routers/merchants/merchant.go
+++ internal/routers/merchants/merchant.go
@@ -7,5 +7,5 @@ import (
 func (r *MerchantRouter) MerchantRouter(router *gin.Engine) {
 	router.POST("/api/merchant/", r.controller.CreateMerchant)
 	router.GET("/api/merchant/:id", r.controller.ReadMerchant)
-	router.PUT("/api/merchant/:id", r.controller.UpdateMerchant)
+	router.PATCH("/api/merchant/:id", r.controller.UpdateMerchant)
 }
